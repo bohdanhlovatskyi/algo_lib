500a6476dfb03c1965b5cdec5ed151ea3f19aec9
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2022-04-27 11:38:15 +0300

    reduce and transform

--- algo_lib/algo_lib.hpp
+++ algo_lib/algo_lib.hpp
@@ -12,7 +12,8 @@ namespace {
             class OutputIt,
             std::enable_if_t<!std::is_trivially_copyable_v<
                     typename std::iterator_traits<InputIt>::value_type>, bool> = true>
-    constexpr OutputIt copy_(InputIt first, InputIt last, OutputIt o_first, std::input_iterator_tag) {
+    constexpr OutputIt copy_(InputIt first, InputIt last,
+                             OutputIt o_first, std::input_iterator_tag) {
         // call copy assignment for each of the elements
         for (; first != last; ++first, ++o_first) {
             *o_first = *first;
@@ -31,7 +32,8 @@ namespace {
             class OutputIt,
             std::enable_if_t<std::is_trivially_copyable_v<
                     typename std::iterator_traits<InputIt>::value_type>, bool> = true>
-    constexpr OutputIt copy_(InputIt first, InputIt last, OutputIt o_first, std::input_iterator_tag) {
+    constexpr OutputIt copy_(InputIt first, InputIt last,
+                             OutputIt o_first, std::input_iterator_tag) {
         using underlying_type = typename std::iterator_traits<InputIt>::value_type;
 #if defined(__STDC_LIB_EXT1__)
         memcpy_s(&*o_first, &*first, (last - first) * sizeof(underlying_type));
@@ -48,7 +50,8 @@ namespace ml {
             std::enable_if_t<!std::is_trivially_copyable_v<
                             typename std::iterator_traits<InputIt>::value_type>, bool> = true>
     constexpr OutputIt copy(InputIt first, InputIt last, OutputIt o_first) {
-        copy_(first, last, o_first, typename std::iterator_traits<OutputIt>::iterator_category());
+        copy_(first, last, o_first,
+              typename std::iterator_traits<OutputIt>::iterator_category());
         return o_first;
     }
 
@@ -63,27 +66,67 @@ namespace ml {
             std::enable_if_t<std::is_trivially_copyable_v<
                     typename std::iterator_traits<InputIt>::value_type>, bool> = true>
     constexpr OutputIt copy(InputIt first, InputIt last, OutputIt o_first) {
-        copy_(first, last, o_first, typename std::iterator_traits<OutputIt>::iterator_category());
+        copy_(first, last, o_first,
+              typename std::iterator_traits<OutputIt>::iterator_category());
         return o_first;
     }
 
-    template<class InputIt,
-            class OutputIt,
-            class UnaryOperation>
-    constexpr OutputIt transform( InputIt first1,
-                                  InputIt last1,
-                                  OutputIt d_first,
-                                  UnaryOperation unary_op);
+    template<class InputIt, class OutputIt, class UnaryOperation>
+    constexpr OutputIt transform( InputIt first, InputIt last,
+                                  OutputIt d_first, UnaryOperation unary_op) {
 
-    template<class InputIt1,
-            class InputIt2,
-            class OutputIt,
-            class BinaryOperation >
-    constexpr OutputIt transform( InputIt1 first1,
-                                  InputIt1 last1,
-                                  InputIt2 first2,
-                                  OutputIt d_first,
-                                  BinaryOperation binary_op);
+        while (first != last) {
+            *d_first = unary_op(*first);
+            ++first; ++d_first;
+        }
+
+        return d_first;
+    }
+
+    template<class InputIt1, class InputIt2, class OutputIt, class BinaryOp>
+    constexpr OutputIt transform( InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                                  OutputIt d_first, BinaryOp bin_op) {
+
+        while (first1 != last1) {
+            *d_first = bin_op(*first1, *first2);
+            ++first1; ++first2; ++d_first;
+        }
+
+        return d_first;
+    }
+
+    template<class InputIt, class BinaryOperation>
+    constexpr decltype(auto) reduce( InputIt first, InputIt last,
+                           BinaryOperation reducer) {
+        using T = typename std::iterator_traits<InputIt>::value_type;
+
+        T result = *first++;
+        while (first != last) {
+            result = reducer(result, *first);
+            first++;
+        }
+
+        return result;
+    }
+
+    template<class InputIt, class UnaryOperatrion, class BinaryOperation>
+    constexpr decltype(auto) map_reduce( InputIt first, InputIt last,
+                                         UnaryOperatrion mapper,
+                                         BinaryOperation reducer)
+    {
+        using UT = typename std::iterator_traits<InputIt>::value_type;
+        using diff = typename std::iterator_traits<InputIt>::difference_type;
+
+        const diff size = last - first;
+        UT* tmp = (UT* ) ::operator new(size * sizeof(UT));
+
+        ml::transform(first, last, tmp, mapper);
+        auto res = reduce(tmp, tmp + size, reducer);
+
+        operator delete(tmp);
+
+        return res;
+    }
 }
 
 #endif // MY_ALGO_LIB__
--- main.cpp
+++ main.cpp
@@ -6,18 +6,51 @@
 
 #include "algo_lib.hpp"
 
-int main(int argc, char* argv[]) {
-    (void) argc; (void) argv;
-    std::vector<int> test = {2, 3, 4, 3};
-    std::vector<int> res = {2, };
-    res.resize(test.size());
-
-    ml::copy(test.begin(), test.end(), res.begin());
-    for (const auto& elm : res) {
+template<typename T>
+void print_vec(const std::vector<T>& vec) {
+    for (const auto& elm : vec) {
         std::cout << elm << " ";
     }
     std::cout << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    (void) argc; (void) argv;
+
+    // ---------------------- copy -------------------------
+    std::cout << "copy example: " << std::endl;
+    std::vector<int> test = {2, 3, 4, 3};
+    print_vec(test);
+
+    std::vector<int> res = {};
+    res.resize(4);
+    ml::copy(test.begin(), test.end(), res.begin());
+
+    std::cout << "copied into: " << std::endl;
+    print_vec(res);
+
+    // Won't compile
     // ml::copy(4, 7, res.begin());
+
+    // ---------------------- transform -------------------------
+    std::cout << std::endl << "transform example: " << std::endl;
+    using namespace std::string_literals;
+    auto test2 = {"hello"s, "there"s};
+    std::vector<std::string> res2;
+    res2.resize(2);
+
+    ml::transform(test2.begin(), test2.end(), res2.begin(),
+                  [](const auto& str){ return str + " oopss  ."s; });
+    print_vec(res2);
+
+    // ---------------------- reduce -------------------------
+    std::cout << std::endl << "reduce example: " << std::endl;
+    std::vector<int> test3 = {3, 4, 5};
+
+    auto res3 = ml::map_reduce(test3.begin(), test3.end(),
+                           [](auto& num){ return num * num; },
+                           [](auto& num1, const auto& num2) { return num1 + num2; });
+    std::cout << "reduce result: " << res3 << std::endl;
 
     return 0;
 }
